b3e40b07f7e6ce5ec111a044c635013c2c86f6a3
CantWontDo <ninjarahul19@gmail.com>
2023-12-24 12:53:04 -0800

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CannonShooter
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private GraphicsDevice _device;
+
+        private Texture2D _backgroundTexture;
+
+        private int _screenWidth;
+        private int _screenHeight;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 1080;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+            Window.Title = "Cannon Shooter";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _device = _graphics.GraphicsDevice;
+
+            _backgroundTexture = Content.Load<Texture2D>("background");
+
+            _screenHeight = _graphics.PreferredBackBufferHeight;
+            _screenWidth = _graphics.PreferredBackBufferWidth;
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            DrawScenery();
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        private void DrawScenery()
+        {
+            Rectangle screenRectangle = new Rectangle(0, 0, _screenWidth, _screenHeight);
+            _spriteBatch.Draw(_backgroundTexture, screenRectangle, Color.White);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new CannonShooter.Game1();
+game.Run();
